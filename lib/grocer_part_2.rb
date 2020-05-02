7fddda781012db16095f3314ca6e9cb96316611f
Bryce Kennedy <btken88@gmail.com>
2020-05-02 15:29:08 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,14 +1,16 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  # Go through each item in both the cart and the coupons list to check if there is a match
   cart.each do |cart_item|
     coupons.each do |coupon_item|
       if cart_item[:item] == coupon_item[:item]
+        # While enough items exist in the cart to use the coupon, do so
+        coupon_price = coupon_item[:cost]/coupon_item[:num]
+        coupon_applied = cart_item
+        coupon_applied[:item] = "#{cart_item[:item]} W/COUPON"
         while cart_item[:count] <= coupon_item[:num] do
-          if find
+          
         end
     end
   end
